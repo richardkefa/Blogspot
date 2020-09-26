3a75650215615222230268761c52541eaab97651
richardkefa <richardkefa@gmail.com>
2020-09-27 02:35:25 +0300

    add password hashing and authentication blueprint

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
 
 
 class User(db.Model):
@@ -9,7 +11,18 @@ class User(db.Model):
   email = db.Column(db.String(),unique = True)
   role = db.Column(db.String(100))
   comments = db.relationship('Comment',backref = 'user',lazy="dynamic")
+  post = db.relationship('BlogPost',backref = 'user',lazy="dynamic")
+  pass_secure = db.Column(db.String())
 
+  @property
+  def password(self):
+    raise AttributeError('You cannot read the password attribute')
+  
+  @password.setter
+  def password(self,password):
+    self.pass_secure = generate_password_hash(password)
+  def verify_password(self,password):
+    return check_password_hash(self.pass_secure,password)
   
   
 class BlogPost(db.Model):
