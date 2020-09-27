01e23f73ccd829cbd973f21d153fde63f5b17174
richardkefa <richardkefa@gmail.com>
2020-09-27 04:08:57 +0300

    add registration form ,route and bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection ='strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -14,6 +19,9 @@ def create_app(config_name):
   
   #Intinilizing flask extensions
   db.init_app(app)
+  login_manager.init_app(app)
+  bootstrap = Bootstrap(app)
+
   
   #Registering the blueprint
   from .main import main as main_blueprint
---
+++ app/auth/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from wtforms import ValidationError
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+  email = StringField('Enter your email address',validators=[Required(),Email()])
+  username = StringField('Enter your username',validators=[Required()])
+  password = PasswordField('Enter password',validators=[Required(),EqualTo('password_confirm',message='passwords must match')])
+  password_confirm = PasswordField('Confirm Passwords',validators=[Required()])
+  submit = SubmitField('Sign Up')
+  
+  def validate_email(self,data_field):
+    if User.query.filter_by(email = data_field.data).first()
+      raise ValidationError('There is an account with that email')
+    
+  def validate_username(self,data_field):
+    if User.query.filter_by(username = data_field.data).first()
+      raise ValidationError('Username is already taken')
