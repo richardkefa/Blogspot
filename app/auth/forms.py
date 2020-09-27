93a5bdc39c670461939d2c73690b45f51250e6c5
richardkefa <richardkefa@gmail.com>
2020-09-27 06:06:18 +0300

    add login form, view route,navbar and base html

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import Required,Email,EqualTo
 from wtforms import ValidationError
 from ..models import User
@@ -12,9 +12,16 @@ class RegistrationForm(FlaskForm):
   submit = SubmitField('Sign Up')
   
   def validate_email(self,data_field):
-    if User.query.filter_by(email = data_field.data).first()
+    if User.query.filter_by(email = data_field.data).first():
       raise ValidationError('There is an account with that email')
     
   def validate_username(self,data_field):
-    if User.query.filter_by(username = data_field.data).first()
+    if User.query.filter_by(username = data_field.data).first():
       raise ValidationError('Username is already taken')
+
+
+class LoginForm(FlaskForm):
+  username = StringField('Enter your username',validators=[Required()])
+  password = PasswordField('Enter password',validators=[Required()])
+  remember = BooleanField('Remember me')
+  submit = SubmitField('Login')
---
+++ start.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY ='f9ur923u09r2ur09'
+
+python3.6 manage.py server
